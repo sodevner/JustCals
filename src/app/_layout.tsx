3fd9defd62caef42407bbc676cd677525bae7d4d
sodevner <soner.bo@outlook.de>
2025-10-15 23:18:50 +0200

    Supabase function deleted because i implemented the guest feature wrong

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,47 +1,9 @@
-import { useEffect, useState } from "react";
-import { ActivityIndicator, Platform, View } from "react-native";
-import { supabase } from "../core/utils/supabase";
-import { ensureGuestUser } from "../data/services/guestUserService";
+import { Platform } from "react-native";
 import RootLayoutAndroid from "../presentation/components/layout/rootlayout.android";
 import RootLayoutiOS from "../presentation/components/layout/rootlayout.ios26";
-import OnboardingScreen from "../presentation/screens/onboarding/onboarding-screen";
 
 export default function RootLayout() {
-  const [loading, setLoading] = useState(true);
-  const [hasProfile, setHasProfile] = useState(false);
-
   const isIOS = Platform.OS === "ios" && Platform.Version >= "26";
-
-  useEffect(() => {
-    const init = async () => {
-      try {
-        const userId = await ensureGuestUser();
-        const { data: profile } = await supabase
-          .from("profiles")
-          .select("*")
-          .eq("id", userId)
-          .single();
-
-        setHasProfile(!!profile?.weight);
-      } catch (error: any) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    init();
-  }, []);
-
-  if (loading) {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
-  }
-
-  if (!hasProfile) return <OnboardingScreen />;
 
   return isIOS ? <RootLayoutiOS /> : <RootLayoutAndroid />;
 }
--- src/data/datasources/asyncstorage.ts
+++
@@ -1,9 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-export const storeGuestUserId = async (id: string) => {
-  await AsyncStorage.setItem('guest_user_id', id);
-};
-
-export const getGuestUserId = async () => {
-  return await AsyncStorage.getItem('guest_user_id');
-};
